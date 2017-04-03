9e44680d92f440dac8d033ddab50e700d0a0f416
Joshua Ho <jho@innocean.ca>
2017-04-03 11:40:33 -0400

    added start script command

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require( "express" );
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === "this_is_a_prototype" ) {
     console.log("Validating webhook");
@@ -8,5 +12,9 @@ app.get('/webhook', function(req, res) {
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);
-  }  
+  }
 });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
